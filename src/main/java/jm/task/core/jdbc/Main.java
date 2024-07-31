9f8665f1cdb3f740f0cbb5c0fa2badded05ecaa7
Vera Pak <verapark.al@icloud.com>
2024-07-31 18:58:24 +0300

    Operations using JDBC

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,41 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
+    private static UserServiceImpl userService = new UserServiceImpl();
+    private static User user1 = new User("Petr", "Petrov", (byte) 34);
+    private static User user2 = new User("Nikolai", "Nikolayev", (byte) 25);
+    private static User user3 = new User("Alina", "Alinova", (byte) 22);
+    private static User user4 = new User("Maha", "Mahovna", (byte) 18);
+
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+
+        // Создание таблицы User(ов)
+        userService.createUsersTable();
+
+        // Добавление 4 User(ов) в таблицу с данными.
+        // После каждого добавления вывод в консоль
+        userService.saveUser(user1.getName(), user1.getLastName(), user1.getAge());
+        System.out.println(String.format("User с именем — %s добавлен в базу данных", user1.getName()));
+
+        userService.saveUser(user2.getName(), user2.getLastName(), user2.getAge());
+        System.out.println(String.format("User с именем — %s добавлен в базу данных", user2.getName()));
+
+        userService.saveUser(user3.getName(), user3.getLastName(), user3.getAge());
+        System.out.println(String.format("User с именем — %s добавлен в базу данных", user3.getName()));
+
+        userService.saveUser(user4.getName(), user4.getLastName(), user4.getAge());
+        System.out.println(String.format("User с именем — %s добавлен в базу данных", user4.getName()));
+
+        // Получение всех User из базы и вывод в консоль
+        System.out.println(userService.getAllUsers());
+
+        // Очистка таблицы User(ов)
+        userService.cleanUsersTable();
+
+        // Удаление таблицы
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,111 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class UserDaoJDBCImpl implements UserDao {
-    public UserDaoJDBCImpl() {
+    public static Logger logger = Logger.getLogger("Logger_UserDaoJDBCImpl");
+    private static Connection connection;
 
+    static {
+        try {
+            connection = Util.getConnection();
+        } catch (SQLException e) {
+            logger.log(Level.WARNING, "Ошибка при попытке установить соединение с БД");
+
+            try {
+                if (!connection.isClosed()) {
+                    connection.close();
+                }
+            } catch (SQLException ex) {
+                logger.log(Level.WARNING, "Ошибка при попытки закрыть соединение");
+            }
+        }
+    }
+
+    public UserDaoJDBCImpl() {
     }
 
     public void createUsersTable() {
-
+        String sql = "CREATE TABLE IF NOT EXISTS users ("
+                + "id BIGINT AUTO_INCREMENT PRIMARY KEY, "
+                + "name VARCHAR(50), "
+                + "lastName VARCHAR(50), "
+                + "age TINYINT)";
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate(sql);
+        } catch (SQLException e) {
+            logger.log(Level.WARNING, "Ошибка при создании таблицы: " + e);
+        }
     }
 
     public void dropUsersTable() {
-
+        String sql = "DROP TABLE IF EXISTS users";
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate(sql);
+        } catch (SQLException e) {
+            logger.log(Level.WARNING, "Ошибка при удалении таблицы: " + e);
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        String sql = "INSERT INTO users (name, lastName, age) VALUES (?, ?, ?);";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setByte(3, age);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            logger.log(Level.WARNING, "Ошибка при сохранении пользователя: " + e);
+        }
     }
 
     public void removeUserById(long id) {
-
+        String sql = "DELETE FROM users WHERE id = ?;";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setLong(1, id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            logger.log(Level.WARNING, "Ошибка при удалении пользователя по ID: " + e);
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> users = new ArrayList<>();
+
+        String sql = "SELECT * FROM users";
+        try (Statement statement = connection.createStatement()) {
+
+            ResultSet resultSet = statement.executeQuery(sql);
+            while (resultSet.next()) {
+                long id = resultSet.getLong("id");
+                String name = resultSet.getString("name");
+                String lastName = resultSet.getString("lastName");
+                byte age = resultSet.getByte("age");
+
+                User user = new User(name, lastName, age);
+                user.setId(id);
+
+                users.add(user);
+            }
+        } catch (SQLException e) {
+            logger.log(Level.WARNING, "Ошибка при получении пользователей: " + e);
+        }
+        return users;
     }
 
     public void cleanUsersTable() {
-
+        String sql = "TRUNCATE TABLE users";
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate(sql);
+        } catch (SQLException e) {
+            logger.log(Level.WARNING, "Ошибка при очистке таблицы: " + e);
+        }
     }
 }
